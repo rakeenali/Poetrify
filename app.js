884a769563465adb5eda3b6178842bbc40a59f71
unknown <rakeenali20@gmail.com>
2019-02-19 12:23:32 +0500

    debuggin heroku

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.use("/api/image", images);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
