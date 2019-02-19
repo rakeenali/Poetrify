b666b0c00ff1f8093db45454f2f01af667ade320
unknown <rakeenali20@gmail.com>
2019-02-19 12:02:38 +0500

    Add changes for heroku

--- app.js
+++ app.js
@@ -35,4 +35,13 @@ app.use("/api/like", like);
 app.use("/api/follow", follow);
 app.use("/api/image", images);
 
-app.listen(5000, () => console.log("Server started on port 5000"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
