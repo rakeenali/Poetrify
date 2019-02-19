1476665be52435968055776f855cce3699320564
unknown <rakeenali20@gmail.com>
2019-02-19 12:41:43 +0500

    debuggin heroku

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === "test") {
   module.exports = require("./testing");
-} else if (process.env.NODE_END === "production") {
+} else if (process.env.NODE_ENV === "production") {
   module.exports = require("./prod");
 } else {
   module.exports = require("./dev");
