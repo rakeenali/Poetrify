5a5192360fb3f72b13a038ac19dceb361e649541
unknown <rakeenali20@gmail.com>
2019-02-19 11:50:53 +0500

    changes in config directory

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
-if(process.env.NODE_ENV === 'test'){
-  module.exports = require('./testing');
-}else{
-  module.exports = require('./dev');
+if (process.env.NODE_ENV === "test") {
+  module.exports = require("./testing");
+} else if (process.env.NODE_END === "production") {
+  module.exports = require("./prod");
+} else {
+  module.exports = require("./dev");
 }
